8b0fa67fbbd6af22e800697529468e2b8c69c51f
Choi-Ye-Seon <choii_ii@naver.com>
2024-07-19 16:46:33 +0900

    visual video 수정

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+// Visual Video
+const video = document.getElementById('background-video')
+const videoContainer = document.getElementById('videoContainer')
+
+// 모바일 저전력모드 함수선언
+function lowPowerMode() {
+    videoContainer.classList.add('low-power-mode');
+}
+
+function videoPlay() {
+    videoContainer.classList.remove('low-power-mode');
+}
+
+// 모바일 저전력모드 함수실행
+// video 일시 중지 시, low-power-mode 클래스 추가
+video.addEventListener('suspend', lowPowerMode);
+// video 재생 시, low-power-mode 클래스 제거
+video.addEventListener('play', videoPlay);
+
+// 저전력모드 감시
+video.play().catch(error => {
+    lowPowerMode();
+});
+
+
 $(document).ready(function() {
     // 성공사례 탭메뉴
     $('.success .tab-menu').click(function() {
