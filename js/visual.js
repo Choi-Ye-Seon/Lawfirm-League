bb9885e97de5e5846b4f3721ae3059b5e701edfe
Choi-Ye-Seon <choii_ii@naver.com>
2024-07-29 15:45:43 +0900

    프로젝트 수정 240729

--- js/visual.js
+++ js/visual.js
@@ -1,43 +1,23 @@
-// video-script.js
+// Visual Video
+const video = document.getElementById('background-video')
+const videoContainer = document.getElementById('videoContainer')
 
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('background-video');
-    const videoContainer = document.getElementById('videoContainer');
+// 모바일 저전력모드 함수선언
+function lowPowerMode() {
+    videoContainer.classList.add('low-power-mode');
+}
 
-    function lowPowerMode() {
-        videoContainer.classList.add('low-power-mode');
-    }
+function videoPlay() {
+    videoContainer.classList.remove('low-power-mode');
+}
 
-    function videoPlay() {
-        videoContainer.classList.remove('low-power-mode');
-    }
+// 모바일 저전력모드 함수실행
+// video 일시 중지 시, low-power-mode 클래스 추가
+video.addEventListener('suspend', lowPowerMode);
+// video 재생 시, low-power-mode 클래스 제거
+video.addEventListener('play', videoPlay);
 
-    // 비디오 요소 로드 확인 및 재생 시도
-    async function attemptPlay() {
-        try {
-            await video.play();
-        } catch (error) {
-            console.error('자동 재생 실패:', error);
-            lowPowerMode();
-        }
-    }
-
-    // 비디오 요소가 로드될 때 자동 재생 시도
-    video.addEventListener('loadeddata', async() => {
-        try {
-            await video.play();
-        } catch (error) {
-            console.error('자동 재생 실패:', error);
-            lowPowerMode();
-        }
-    });
-
-    // 비디오가 재생될 때 저전력모드 클래스 제거
-    video.addEventListener('play', videoPlay);
-
-    // 저전력모드 감시
-    attemptPlay();
-
-    // 비디오 요소 로드 확인 및 재생 시도
-    video.load();
+// 저전력모드 감시
+video.play().catch(error => {
+    lowPowerMode();
 });
